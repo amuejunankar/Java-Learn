aceebaedc03f47a93655d26bb7fc711477bea57f
amuejunankar <55340245+amuejunankar@users.noreply.github.com>
2021-11-04 18:39:28 +0530

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -1,28 +1,78 @@
-import javax.sound.sampled.ReverbType;
+// import javax.sound.sampled.ReverbType;
+
+// public class linearSearch {
+//     public static void main(String[] args) {
+        
+//         int[] nums = {12,14,15,665,44,4,444,20};
+//         int target = 100;
+//         int ans = LinearSearch(nums, target);
+//         System.out.println(ans);
+
+//     }
+
+
+//     static int LinearSearch(int[] arr, int target) {
+//         if (arr.length == 0) {
+//             return -1;
+//         }
+
+//         for (int i = 0; i < arr.length ; i++) {
+//             if ( target == arr[i] ) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+
+//     }
+// }
+
+
+// -----------------------------------------------------------
+
+// ? Sorting array 
+
 
 public class linearSearch {
     public static void main(String[] args) {
         
         int[] nums = {12,14,15,665,44,4,444,20};
-        int target = 100;
-        int ans = LinearSearch(nums, target);
-        System.out.println(ans);
+        
 
-    }
-
-
-    static int LinearSearch(int[] arr, int target) {
-        if (arr.length == 0) {
-            return -1;
-        }
-
-        for (int i = 0; i < arr.length ; i++) {
-            if ( target == arr[i] ) {
-                return i;
+        for( int i = 0; i < nums.length ; i++) {
+            for (int j=i+1; j < nums.length; j++) {
+                int temp = 0;
+                if( nums[i] > nums[j]) {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
             }
+
+            System.out.println(nums[i]);
+            
         }
-
-        return -1;
-
     }
 }
+
+
+
+//------------------------------------------------------------
+// ? Sorting array using inbuilt function()
+
+// import java.util.Arrays;
+// import javax.sound.sampled.ReverbType;
+
+// public class linearSearch {
+//     public static void main(String[] args) {
+        
+//         int[] nums = {12,14,15,665,44,4,444,20};
+//         Arrays.sort(nums);
+
+//         for( int i = 0; i < nums.length ; i++) {
+//             System.out.print(nums[i]+" ");
+//         }
+//     }
+// }
+
+
